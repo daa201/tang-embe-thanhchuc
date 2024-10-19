241871dd95644d765ca2c83104d7a624e2ac38c4
daa201 <139944607+daa201@users.noreply.github.com>
2024-10-19 07:13:39 +0700

    Add files via upload
    Tangembethanhtruc

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Bé Thanh Trúc';
+const giftUrl = 'https://www.facebook.com/PandaCute.Official/';
+const eventName = 'Quà 20-10 của';
+const titleCard = 'Tặng em bé của anh';
+const contentCard = 'Chúc em bé của tui 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bé. Những điều em đang cố gắng sẽ theo ý em. Anh thương em bé anh nhiều! Daa iu em...';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
